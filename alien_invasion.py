550c95918222a37bc542005c04892b33ad33972a
Chris <c_guerra@baylor.edu>
2022-02-28 22:14:26 -0600

    check events

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,16 +27,20 @@ class AlienInvasion:
         #Start the main loop for the game
         while True:
             #Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            
+    def _check_events(self):
+        #Respond to keypresses and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            #Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+        #Redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
-            #Make the most recently drawn screen visible
-            pygame.display.flip()
+        #Make the most recently drawn screen visible
+        pygame.display.flip()
 
 if __name__ == '__main__':
     #Make a game instance, and run the game
