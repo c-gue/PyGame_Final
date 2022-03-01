6fa000a243b278e0258244dbdbd623521ea51a66
Chris <c_guerra@baylor.edu>
2022-03-01 00:19:01 -0600

    creating the _update_bullets() method

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,15 +33,21 @@ class AlienInvasion:
             #Watch for keyboard and mouse events
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-
-            #Get rid of bullets that have diappeared
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-                print(len(self.bullets))
-
+            self._update_bullets()
             self._update_screen()
+            
+    def _update_bullets(self):
+        #Update position of bullets and get rid of old bullets
+        #Update bullet positions
+        self.bullets.update()
+
+        #Get rid of bullets that have diappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+            print(len(self.bullets))
+
+        self._update_screen()
             
     def _check_events(self):
         #Respond to keypresses and mouse events
