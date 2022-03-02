21afdf1fbddb40dc6de75dc3cea0c94fd10782b7
Chris <c_guerra@baylor.edu>
2022-03-02 00:11:00 -0600

    hiding the mouse cursor

--- alien_invasion.py
+++ alien_invasion.py
@@ -114,6 +114,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _check_aliens_bottom(self):
         #Check if any aliens have reached the bottom of the screen
@@ -152,6 +153,9 @@ class AlienInvasion:
             #Create a new fleet and center the ship
             self._create_fleet()
             self.ship.center_ship()
+
+            #Hide the mouse cursor
+            pygame.mouse.set_visible(False)
                 
     def _check_keydown_events(self,event):
         #Respond to keypresses
