379713765779bdf930db40d7de13e7c882b5f4de
Chris <c_guerra@baylor.edu>
2022-03-02 01:05:44 -0600

    displaying the level

--- alien_invasion.py
+++ alien_invasion.py
@@ -92,6 +92,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            #Increase level
+            self.stats.level += 1
+            self.sb.prep_level()
             
     def _update_aliens(self):
             #Update the positions of all aliens in the fleet
@@ -157,6 +161,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             #Get rid of any remaining aliens and bullets
             self.aliens.empty()
